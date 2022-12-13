e1e0e0e5eb7116834416decfb95d36bfa705ddd6
Bilel Belhadj <belhadj19bi@gmail.com>
2022-12-13 11:16:19 -0400

    final commit code working so well

---
+++ include/Secrets.h
@@ -0,0 +1,26 @@
+/*
+SSID du WIFI ainsi que le mot de passe
+Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
+*/
+
+#define WIFI_SECRET_SSID "IDO"
+#define WIFI_SECRET_PASS "99Bidules!"
+
+/*
+Information nécessaire pour le branchement a un Broker MQTT
+Dans le cadre du cours AIIA1013, ne modifié pas cet information
+*/
+
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+#define SECRET_MQTT_SERVER_PORT 1883
+
+/*
+Détails sur l'indentification de l'objet
+Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
+*/
+
+#define SECRET_TOKEN  "cGFLU2DDFzB6PvBE7n50"
+#define SECRET_DEVICE_ID "9dbfafa0-7ae8-11ed-8fd7-e3d583114459"
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,93 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include "WIFIConnector_MKR1000.h"
+#include "MQTTConnector.h" 
+
+#define PIN_LED 6     // Control signal, connect to DI of the LED
+#define NUM_LED 1     // Number of LEDs in a strip
+
+const int BouttonA = 1;
+const int BouttonB = 2;
+const int BouttonC = 3;
+
+Adafruit_NeoPixel RGB_Strip = Adafruit_NeoPixel(NUM_LED, PIN_LED, NEO_GRB + NEO_KHZ800);
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1400;
+int BtA = 0, BtB = 0, BtC = 0;
+int RedLight = 0, GreenLight = 0, BlueLight = 0, ledBright = 0;
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint16_t wait) {
+  for (uint16_t i = 0; i < RGB_Strip.numPixels(); i++) {
+    RGB_Strip.setPixelColor(i, c);
+    RGB_Strip.show();
+    delay(wait);
+  }
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  RGB_Strip.begin();
+  RGB_Strip.show();
+  RGB_Strip.setBrightness(200);    // Set brightness, 0-255 (darkest - brightest)
+
+  pinMode(BouttonA, INPUT);
+  pinMode(BouttonB, INPUT);
+  pinMode(BouttonC, INPUT);
+
+  wifiConnect();                  
+  MQTTConnect(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  ClientMQTT.loop(); 
+  if ((millis() - lastTime) > timerDelay) {
+
+    if (statusR == "true")
+    {
+      RedLight = 255;
+    }else{
+      RedLight = 0;
+    }
+    
+    if (statusG == "true")
+    {
+      GreenLight = 255;
+    }else{
+      GreenLight = 0;
+    }
+
+    if (statusB == "true")
+    {
+      BlueLight = 255;
+    }else{
+      BlueLight = 0;
+    }
+
+    //intensitee de LED
+    ledBright = ledBrightFl;
+    RGB_Strip.setBrightness(ledBright); 
+
+    //allumer la led
+    colorWipe(RGB_Strip.Color(RedLight, GreenLight, BlueLight), 1000);  
+
+    //Statu des bouttons
+    BtA = digitalRead(BouttonA); 
+    BtB = digitalRead(BouttonB); 
+    BtC = digitalRead(BouttonC);
+
+    //send to thingsboard
+    appendPayload("BT_R", BtA);
+    appendPayload("BT_G", BtB);
+    appendPayload("BT_B", BtC);
+    sendPayload();
+
+    lastTime = millis();
+  }
+
 }
